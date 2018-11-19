8678283418921a3564f4a1f5393e78005ce26b1a
Yongjin <yjjiangphysics@gmail>
2018-11-19 01:11:41 -0600

    commenting out the twitter-related part

--- scrapy_mars.py
+++ scrapy_mars.py
@@ -24,13 +24,13 @@ import os
 import json
 import tweepy 
 import sys
-sys.path.append('/Users/jyj/Dropbox/A_A_Data_Analysis/Group_Projects')
+#sys.path.append('/Users/jyj/Dropbox/A_A_Data_Analysis/Group_Projects')
 # Import Twitter API Keys
-from config import consumer_key, consumer_secret, access_token, access_token_secret
-# Setup Tweepy API Authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+# from config import consumer_key, consumer_secret, access_token, access_token_secret
+# # Setup Tweepy API Authentication
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
+# api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
 
@@ -50,14 +50,7 @@ def scrape():
             latest_title
             latest_text
             latest_news={"title":latest_title,"text":latest_text}
-            # i=0;
-            # for result in results:
-            #         i+=1
-            #         print(i,'. ','title=',result.select('h3')[0].get_text());
-            #         print(i,'. ', 'text=',result.select('div.article_teaser_body')[0].get_text());
-
-
-            # In[193]:
+           
 
 
             #JPL Mars Space Images - Featured Image: this code block will generate featured_image_url
@@ -73,14 +66,6 @@ def scrape():
             browser.visit(featured_image_url)
 
 
-            # In[ ]:
-
-
-
-
-
-            # In[194]:
-
 
             #Mars Weather: this code block will get latest mars_weather mars_weather from twitter
             #method 1: web scraping 
@@ -89,9 +74,9 @@ def scrape():
             soup = BeautifulSoup(response.text, 'html.parser')
             mars_weather = soup.select("li.js-stream-item")[0].select("p.TweetTextSize")[0].text
             mars_weather
-            # method2: get results by twitter api:
-            public_tweets = api.user_timeline("@MarsWxReport")
-            public_tweets[0]['text']
+            # # method2: get results by twitter api:
+            # public_tweets = api.user_timeline("@MarsWxReport")
+            # public_tweets[0]['text']
 
 
             # In[195]:
@@ -109,11 +94,7 @@ def scrape():
             col2=[a.rstrip('\n') for a in col2]
             facts_dict = dict(zip(col1,col2))
             facts_dict
-            # facts_dict_keys=list(facts_dict.keys())
-            # facts_dict_values=list(facts_dict.values())
-            # facts_dict_keys.pop()
-            # facts_dict_values.pop()  
-
+            
             #######
             tables = pd.read_html(facts_url)
             facts_df=tables[0].rename(columns={0:" ",1:"Value"}).set_index(" ")
@@ -171,17 +152,7 @@ def scrape():
             results=collection.find()
             results[0]
 
-            # collection = db.Mars_facts_dict_keys
-            # collection.drop()
-            # collection.insert_one(facts_dict_keys)
-            # results=collection.find()
-            # results[0]
-
-            # collection = db.Mars_facts_dict_values
-            # collection.drop()
-            # collection.insert_one(facts_dict_values)
-            # results=collection.find()
-            # results[0]
+            
 
             collection = db.Mars_weather
             collection.drop()
@@ -202,56 +173,7 @@ def scrape():
             results[0]
             client.close()
 
-            # In[215]:
-
-
-            # listings = db.Mars.find()
-
-            # for listing in listings:
-            #     print(listing)
-
-
-            # In[137]:
-
-
-            # "https://astrogeology.usgs.gov"+"/cache/images/cfa62af2557222a02478f1fcd781d445_cerberus_enhanced.tif_full.jpg"
-            # "https://astrogeology.usgs.gov"+"/cache/images/dfaf3849e74bf973b59eb50dab52b583_cerberus_enhanced.tif_thumb.png"
-
-
-            # In[ ]:
-
+            return None
 
             # import scrapy
-
-
-            # class MofanSpider(scrapy.Spider):
-            #     name = "James"
-            #     start_urls = [
-            #       "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-
-            #     ]
-            #     # unseen = set()
-            #     # seen = set()      # we don't need these two as scrapy will deal with them automatically
-
-            #     def parse(self, response):
-            #         yield {     # return some results
-            #             'title': response.css('h1::text').extract_first(default='Missing').strip().replace('"', ""),
-            #             'url': response.url,
-            #         }
-
-            #         urls = response.css('a::attr(href)').re(r'^/.+?/$')     # find all sub urls
-            #         for url in urls:
-            #             yield response.follow(url, callback=self.parse)     # it will filter duplication automatically
-
-
-            # # lastly, run this in terminal
-            # # scrapy runspider 5-2-scrapy.py -o res.json
-            # %%bash
-            # scrapy runspider James_scrapy.py -o res.json
-
-
-            # In[ ]:
-
-
-            #Step 2 - MongoDB and Flask Application
 
