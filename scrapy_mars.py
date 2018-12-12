8c1e5b812a1548a6d0e63ff48189f14b9c4d9d13
Yongjin <yjjiangphysics@gmail>
2018-12-12 00:22:36 -0600

    print str0,str1

--- scrapy_mars.py
+++ scrapy_mars.py
@@ -124,7 +124,9 @@ def scrape():
 
 
             str0=os.environ.get("MONGOLAB_URI")
+            print("str0=",str0)
             str1=str0.split('/');str1[-1]='Mars_db'
+            print("str1=",str1)
             str0='/'.join(str1)
            
             conn=str0
