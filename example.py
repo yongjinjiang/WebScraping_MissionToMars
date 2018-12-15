c8a56b04730ca7ebcc5cb44071151ab0671c8742
Yongjin <yjjiangphysics@gmail>
2018-12-15 12:36:01 -0600

    try simple app

---
+++ example.py
@@ -0,0 +1,13 @@
+import scrapy
+class TestSpider(scrapy.Spider):
+    name = "test"
+
+    start_urls = [
+    "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+,
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-1] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
