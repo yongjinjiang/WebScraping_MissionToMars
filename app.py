94f54fe60171bb8f82798e9f9a6c19a5ee7d1d4a
Yongjin <yjjiangphysics@gmail>
2018-12-15 13:02:25 -0600

    add flask

--- app.py
+++ app.py
@@ -35,10 +35,29 @@ soup = BeautifulSoup(html, 'html.parser')
 # In[4]:
 
 
-len(soup)
+#len(soup)
+
+
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from scrapy_mars import scrape
+import os
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/Mars_db"
 
 
 # In[ ]:
+@app.route("/")
+def index():
+   # listings = mongo.db.Mars.find_one()
+   return "hello"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
