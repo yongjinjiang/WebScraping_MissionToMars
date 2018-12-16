d0fb819c350a3c027344763fab58b6eebae36f43
Yongjin <yjjiangphysics@gmail>
2018-12-15 23:17:58 -0600

    simple app.py (print numbers)

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+Python app.py
--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-from flask import Flask, render_template, redirect
-# from flask_pymongo import PyMongo
-# from scrapy_mars import scrape
-# import os
+import time
 
-app = Flask(__name__)
+i=0
+while i<5000:
+   i+=1
+   print(i) 
+   
+   time.sleep(2)
 
-# Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/Mars_db"
-
-
-# In[ ]:
-@app.route("/")
-def index():
-   # listings = mongo.db.Mars.find_one()
-   return "hello"
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+  
