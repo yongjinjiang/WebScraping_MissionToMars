4ee28dbd10a54d03a9d5ca46b0fffd07c69913a0
Yongjin <yjjiangphysics@gmail>
2018-12-15 22:56:22 -0600

    only flask route: try heroku deploy

--- app.py
+++ app.py
@@ -1,47 +1,7 @@
-# Dependencies
-from bs4 import BeautifulSoup
-import requests
-import pymongo
-from splinter import Browser
-import pandas as pd
-import numpy as np
-import re
-import os
-###twitter
-import json
-
-
-# In[2]:
-
-
-## https://splinter.readthedocs.io/en/latest/drivers/chrome.html
-#get_ipython().system('which chromedriver')
-#
-
-# In[3]:
-
-
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-# browser = Browser('chrome', **executable_path, headless=False)
-# browser = Browser('django')
-browser = Browser('chrome', **executable_path, headless=False)
-url="https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-
-browser.visit(url)
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[4]:
-
-
-#len(soup)
-
-
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-from scrapy_mars import scrape
-import os
+# from flask_pymongo import PyMongo
+# from scrapy_mars import scrape
+# import os
 
 app = Flask(__name__)
 
@@ -58,7 +18,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
